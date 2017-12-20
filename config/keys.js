b9213df76bdf6d681772411889bfd942383ad59a
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-20 23:12:19 +0000

    Test mongoose oauth

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,9 @@
-module.exports = {
-	googleClientID: '985096495984-ftmm812ornooc7quu3d4gniv67lid9h7.apps.googleusercontent.com',
-	googleClientSecret: 'mNn0wX6k92E3neUIFjviWiQN',
-	mongoURI: 'mongodb://wilsonsky:baylor24@ds161580.mlab.com:61580/webcrawler',
-	cookieKey: 'adasdasdasdasdadDAAdasdDASDadDAsdasdadFDGfdgd'
-};
+// Figure out which set of credentials to return
+if(process.env.NODE_ENV === 'production') {
+	// return production keys
+	module.exports = require('./prod');
+
+} else {
+	// return dev keys
+	module.exports = require('./dev');
+}
