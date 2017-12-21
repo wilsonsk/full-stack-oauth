a6184adb9eebd07425ed6eb957eba74a98f42830
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-21 08:42:09 +0000

    Update async

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,6 +3,7 @@ import { FETCH_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
 	// redux-thunk: purpose is to inspect whatever value is returned from this action creator -- if function is returned (as opposed to normal action), then redux thunk will automatiically call the returned function and pass in the dispatch function as an argument where the action is forwarded off to all reducers
-	dispatch({ type: FETCH_USER, payload: await axios.get('/api/current_user'});
+	const res = await axios.get('/api/current_user');
+	dispatch({ type: FETCH_USER, payload: res.data });
 }
 
