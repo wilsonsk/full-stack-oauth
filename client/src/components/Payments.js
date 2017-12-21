c30caee22a52489733d8eb2d3cad254507eb17ca
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-21 23:10:20 +0000

    Test handleToken()

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
 
 class Payments extends Component { 
 	render() {
@@ -8,7 +11,7 @@ class Payments extends Component {
 				name="SmartExchange"
 				description="$1 for Int'l Exchange"
 				amount={100}
-				token={token => console.log(token)}
+				token={token => this.props.handleToken(token)}
 				stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}
 			>
 				<button className="btn">
@@ -20,4 +23,4 @@ class Payments extends Component {
 
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
