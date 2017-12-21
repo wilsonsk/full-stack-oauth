8fee83467a380c69d4851c092c0b7810e1b1921d
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-21 05:44:01 +0000

    Test intial redux

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-	  <h2>Hello Full Stack</h2>
+	  <h1>Hello Full Stack</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -17,7 +12,6 @@ class App extends Component {
 	<a href="/auth/google">Sign in With Google</a>
       </div>
     );
-  }
 }
 
 export default App;
