a55a56c5ee0dbd2d4983ce381f63a0e7e748402e
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-21 08:40:06 +0000

    Update ES6 async refactor

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,7 +3,6 @@ import { FETCH_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
 	// redux-thunk: purpose is to inspect whatever value is returned from this action creator -- if function is returned (as opposed to normal action), then redux thunk will automatiically call the returned function and pass in the dispatch function as an argument where the action is forwarded off to all reducers
-	const res = axios.get('/api/current_user');
-	dispatch({ type: FETCH_USER, payload: res });
+	dispatch({ type: FETCH_USER, payload: await axios.get('/api/current_user'});
 }
 
