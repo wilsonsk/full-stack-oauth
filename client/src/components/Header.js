7b54aa5cf2699aaba0c5630badd314afd2e90ca7
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-21 20:16:52 +0000

    test header state

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
 	renderContent() {
@@ -29,9 +29,9 @@ class Header extends Component {
 		return (
 			<nav>
 				<div className="nav-wrapper">
-					<a className="left brand-logo">
-						Full Stack Oauth
-					</a>
+					<Link to={this.props.auth ? '/surveys'  : '/'} className="left brand-logo">
+						SmartExchange
+					</Link>
 					<ul className="right">
 						{this.renderContent()}
 					</ul>
