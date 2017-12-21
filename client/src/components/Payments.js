953ea7adda91c5ce153990703c64c1d20439dc22
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-21 22:44:23 +0000

    Test stripe style

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,8 +5,6 @@ class Payments extends Component {
 	render() {
 		return (
 			<StripeCheckout 
-				name="SmartExchange"
-				description="$1 to Exchanged"
 				amount={100}
 				token={token => console.log(token)}
 				stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}
