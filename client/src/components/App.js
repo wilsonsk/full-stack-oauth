499b173f99b254418f3d54bb31136463285e5079
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-21 08:17:58 +0000

    Test redux

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route  } from 'react-router-dom';
 import { connect } from 'react-redux';
-import * as actions from './actions';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>
