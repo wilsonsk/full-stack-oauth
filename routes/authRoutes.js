9edd3c23efa331ca35238c4b75b3c939801ba4e7
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-21 19:46:32 +0000

    Test auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,7 @@ module.exports = app => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'),(res, req) => {
+  app.get('/auth/google/callback', passport.authenticate('google'),(req, res) => {
 	res.redirect('/surveys');
   });
 
