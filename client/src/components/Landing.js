5bea1606fa08416368fa2001abc8c69cefd0f531
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-21 20:08:27 +0000

    test landing

---
+++ client/src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Landing = () => {
+	return (
+		<div className={{ display: 'flex' }}>
+			<div>
+				<h1>This is a heroshot</h1>	
+			</div>
+			<div>
+				<h1>This is a login CTA</h1>
+			</div>
+		</div>
+	);
+};
+
+export default Landing;
