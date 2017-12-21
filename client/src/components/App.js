febb086787cc778c03a60e5adb9da160bff01b52
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-21 06:25:16 +0000

    Testing router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route  } from 'react-router-dom';
 
 const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
-const SurveryNew = () => <h2>SurveyNew</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
 const App = () => {
