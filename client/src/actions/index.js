59c4ba296b6af6a0bcbc1c255566344ba1044795
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-21 08:04:59 +0000

    Test redux thunk

---
+++ client/src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+const fetchUser = () => {
+	// redux-thunk: purpose is to inspect whatever value is returned from this action creator -- if function is returned (as opposed to normal action), then redux thunk will automatiically call the returned function and pass in the dispatch function as an argument where the action is forwarded off to all reducers
+	return function(dispatch) {
+		axios.get('/api/current_user').then(res => {
+			dispatch({ type: FETCH_USER, payload: res })
+		});
+	}	
+}
+
+export default fetchUser;
