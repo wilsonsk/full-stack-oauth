d4765c22bddd37020940bf7a0936b6e398eee3ca
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-22 00:19:41 +0000

    Test header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,7 +20,7 @@ class Header extends Component {
 			default:
 				return [
 					<li key="0"><Payments /></li>,
-					<li key="1">Credits: {this.props.auth.credits}</li>
+					<li key="1" style+{{ margin: '0 10px' }}>Credits: {this.props.auth.credits}</li>,
 					<li key="2"><a href="/api/logout">Log Out</a></li>
 				];
 		}
