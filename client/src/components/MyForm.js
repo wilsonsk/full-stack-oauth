8d2a8bc2605300d319c61e9e8233aecba8e46a72
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-23 17:08:34 +0000

    Test form

--- client/src/components/MyForm.js
+++ client/src/components/MyForm.js
@@ -1,27 +1,18 @@
-import React, { Component} from 'react';
-import { Field, reduxForm } from 'redux-form';
-import { Button, Header, Icon, Modal, Transition, Form, Input, Select, TextArea, Dropdown } from 'semantic-ui-react';
+import React from 'react'
+import { Button, Checkbox, Form } from 'semantic-ui-react'
 
-class MyForm extends Component {
-	submit(values, dispatch) {
-		console.log(values);
-	}
+const MyForm = () => (
+  <Form>
+    <Form.Field>
+      <label>First Name</label>
+      <input placeholder='You send' />
+    </Form.Field>
+    <Form.Field>
+      <label>Last Name</label>
+      <input placeholder='Recipients gets' />
+    </Form.Field>
+    <Button type='submit'>Submit</Button>
+  </Form>
+)
 
-	render() {
-		return (
-			<Form onSubmit={this.submit()}>
-				<h3>How much would you like to transfer</h3>
-				<Form.group>
-					<Field control={Input} label="$" placeholder="You send" /> 
-					<Field control={Input} label="$" placeholder="Recipient gets" /> 
-				</Form.group>
-				<Button type="submit" content='Submit Info' icon='add' labelPosition='right' color="green" />
-			</Form>
-		);
-	}
-
-}
-
-export default reduxForm({
-	form: 'Exchange'
-})(MyForm);
+export default MyForm;
