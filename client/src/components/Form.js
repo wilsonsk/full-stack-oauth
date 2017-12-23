fef23577339b2c5729ece40179cad369c7e94da7
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-23 09:56:55 +0000

    Test form

---
+++ client/src/components/Form.js
@@ -0,0 +1,37 @@
+import React, { Component} from 'react';
+import { Button, Header, Icon, Modal, Transition, Form, Input, Select, TextArea, Dropdown } from 'semantic-ui-react';
+
+class Form extends Component {
+	submit(values, dispatch) {
+		console.log(values);
+	}
+
+	youSendChange(event, data) {
+	render() {
+		return (
+			<Transition animation="fade">
+				<Modal trigger={<a><Icon name="add" /></a>} closeIcon>
+					<Header content="Create an exchange" />
+					<Modal.Content>
+						<Form onSubmit={this.submit()}>
+							<h3>How much would you like to transfer</h3>
+							<Form.group>
+								<Field control={Input} label="$" placeholder="You send" /> 
+								<Field control={Input} label="$" placeholder="Recipient gets" /> 
+							</Form.group>
+							<Button type="submit" content='Submit Info' icon='add' labelPosition='right' color="green" />
+						</Form>
+					</Modal.Content>
+					<Modal.Actions>
+	
+					</Modal.Actions>
+				</Modal>
+			</Transition>
+		);
+	}
+
+}
+
+export default reduxForm({
+	form: 'Exchange'
+})(Form);
