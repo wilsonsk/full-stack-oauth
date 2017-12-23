940e4f2a1f5bda270e53994e7fea6e7387c0083b
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-23 16:54:10 +0000

    Test Form

--- client/src/components/MyForm.js
+++ client/src/components/MyForm.js
@@ -9,24 +9,14 @@ class MyForm extends Component {
 
 	render() {
 		return (
-			<Transition animation="fade">
-				<Modal trigger={<Button>SmartExchange</Button>}>
-					<Header content="Create an exchange" />
-					<Modal.Content>
-						<Form onSubmit={this.submit()}>
-							<h3>How much would you like to transfer</h3>
-							<Form.group>
-								<Field control={Input} label="$" placeholder="You send" /> 
-								<Field control={Input} label="$" placeholder="Recipient gets" /> 
-							</Form.group>
-							<Button type="submit" content='Submit Info' icon='add' labelPosition='right' color="green" />
-						</Form>
-					</Modal.Content>
-					<Modal.Actions>
-	
-					</Modal.Actions>
-				</Modal>
-			</Transition>
+			<Form onSubmit={this.submit()}>
+				<h3>How much would you like to transfer</h3>
+				<Form.group>
+					<Field control={Input} label="$" placeholder="You send" /> 
+					<Field control={Input} label="$" placeholder="Recipient gets" /> 
+				</Form.group>
+				<Button type="submit" content='Submit Info' icon='add' labelPosition='right' color="green" />
+			</Form>
 		);
 	}
 
