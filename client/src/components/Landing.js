da46a8008462e791370b29a0085a479a41b71644
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-24 06:15:59 +0000

    Test inline styles (form)

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import MyForm from './form/MyForm';
 
+const gridStyle = {
+	display: 'flex',
+	margin: '-10px 0 10px -10px',
+};
+
+const cellStyle = {
+	padding: '10px 0 0 10px',
+	height: '100%',
+	background: 'blue',
+	padding: '10px'
+};
+
 const Landing = () => {
 	return (
-		<div className={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', alignItems: 'center', }}>
-			<div className={{ flex: 1 }}>
+		<div className={gridStyle}>
+			<div className={cellStyle}>
 				<h1>This is a heroshot</h1>	
 			</div>
-			<div className={{ flex:1 }}>
+			<div className={cellStyle}>
 				<h1>This is a login CTA</h1>
 				<MyForm />
 			</div>
