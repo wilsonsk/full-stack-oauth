9f1d4f4bc8c888cb46adfb7cc968475de589c780
Ubuntu <ubuntu@ip-172-31-40-203.us-west-2.compute.internal>
2017-12-24 05:07:22 +0000

    Test css

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,10 +4,10 @@ import MyForm from './form/MyForm';
 const Landing = () => {
 	return (
 		<div className={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', alignItems: 'center', }}>
-			<div>
+			<div className={{ flex: 1 }}>
 				<h1>This is a heroshot</h1>	
 			</div>
-			<div>
+			<div className={{ flex:1 }}>
 				<h1>This is a login CTA</h1>
 				<MyForm />
 			</div>
