3c6122c2b4234c895817cd642a1ed49ac7d41ae4
root <root@localhost.localdomain>
2017-12-31 12:17:32 -0800

    Test actions

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import MyForm from './form/MyForm';
 
-const Landing = () => {
-	return (
-		<div style={{ display: 'flex', border: 'solid', alignItems: 'center'}}>
-				<div style={{ border: 'solid', flex: '1.618', flexDirection: 'column' }}>
-					<h1>This is a heroshot</h1>	
-				</div>
-				<div style={{ border: 'solid', flex: '1', flexDirection: 'column' }}>
-					<MyForm />
-				</div>
-		</div>
-	);
+import * as actions from '../actions';
+
+class Landing extends Component {
+	componentWillMount() {
+		this.props.fetchPhotos();
+		console.log(this.props.photos);
+	};
+
+	render() {
+		return (
+			<div style={{ display: 'flex', border: 'solid', alignItems: 'center'}}>
+					<div style={{ border: 'solid', flex: '1.618', flexDirection: 'column' }}>
+						<h1>This is a heroshot</h1>	
+					</div>
+					<div style={{ display:'flex', border: 'solid', flex: '1', flexDirection: 'column' }}>
+						<div style={{ border: 'solid', flex: '1' }}>
+							<h1>This is a CTA</h1>	
+						</div>
+						<MyForm />
+					</div>
+			</div>
+		);
+	}
 };
 
-export default Landing;
+function mapStateToProps(state) {
+	return { photos: state.photos };
+}
+
+export default connect(mapStateToProps, actions)(Landing);
