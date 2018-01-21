34d49f2cb9c3c4e6b8ac214d0b2f11def8f346da
root <root@localhost.localdomain>
2018-01-21 10:33:37 -0800

    Add animations to features

--- client/src/components/presentational/hero/cta/Cta.js
+++ client/src/components/presentational/hero/cta/Cta.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Cta = styled.section`
   margin-top: 90px;
   display: grid;
-  grid-template-columns: 1.618fr auto;
+  grid-template-columns: 1.618fr 1;
   transition: 'background-color 5s';
   position: relative;
 `;
