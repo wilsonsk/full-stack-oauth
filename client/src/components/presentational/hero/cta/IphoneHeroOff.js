ddff54a3bdbde74f29b1272c730e395d9ea525d2
root <root@localhost.localdomain>
2018-01-22 08:47:15 -0800

    Add features section and animations

--- client/src/components/presentational/hero/cta/IphoneHeroOff.js
+++ client/src/components/presentational/hero/cta/IphoneHeroOff.js
@@ -15,7 +15,6 @@ const IphoneHeroOff = styled.div`
   position: absolute;
   opacity: ${props => props.animated ? '1' : '0'};
   margin-bottom: -240px;
-  padding-right: 10%;
   width: 100%;
   animation: ${props => props.animated ? `
 		${slideUp} 3s ease-out forwards, 
