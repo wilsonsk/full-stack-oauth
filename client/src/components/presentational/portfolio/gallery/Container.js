7707e2f0d38088aefa55f95f180fc901bcf5bd46
root <root@localhost.localdomain>
2018-01-30 17:21:38 -0800

    Add infinite scroll + dynamic masonry layout

--- client/src/components/presentational/portfolio/gallery/Container.js
+++ client/src/components/presentational/portfolio/gallery/Container.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  max-width: 1100px;
+  max-width: 1500px;
   margin: 0 auto;  
   padding: 2% 2%;
   overflow: hidden;
